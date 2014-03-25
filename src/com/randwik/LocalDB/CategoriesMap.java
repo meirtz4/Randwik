570011c7d78ba163c78bbfa0a80cfba0b09597cd
Meir <Meirtz4@gmail.com>
2014-03-25 08:11:27 +0200

    Added\Updated missing categories

--- src/com/randwik/LocalDB/CategoriesMap.java
+++ src/com/randwik/LocalDB/CategoriesMap.java
@@ -32,21 +32,31 @@ public class CategoriesMap extends HashMap<String, AbstractCategory> implements 
 		/*******************************************/
 		/** Here we should add ALL the categories **/
 		/*******************************************/
+		this.put("Agriculture", new AgricultureCategory());
 		this.put("Arts", new ArtsCategory());
 		this.put("Business", new BusinessCategory());
+		this.put("Chronology", new ChronologyCategory());
+		this.put("Concepts", new ConceptsCategory());
+		this.put("Culture", new CultureCategory());
+		this.put("Education", new EducationCategory());
 		this.put("Environment", new EnvironmentCategory());
+		this.put("Geography", new GeographyCategory());
+		this.put("Health", new HealthCategory());
 		this.put("History", new HistoryCategory());
+		this.put("Humanities", new HumanitiesCategory());
+		this.put("Humans", new HumansCategory());
+		this.put("Language", new LanguageCategory());
 		this.put("Law", new LawCategory());
+		this.put("Life", new LifeCategory());
 		this.put("Mathematics", new MathematicsCategory());
 		this.put("Medicine", new MedicineCategory());
 		this.put("Nature", new NatureCategory());
-		this.put("Society", new SocietyCategory());
-		this.put("Science", new ScienceCategory());
+		this.put("People", new PeopleCategory());
 		this.put("Politics", new PoliticsCategory());
-		this.put("Technology", new TechnologyCategory());
-		this.put("Geography", new GeographyCategory());
-		this.put("Health", new HealthCategory());
+		this.put("Science", new ScienceCategory());
+		this.put("Society", new SocietyCategory());
 		this.put("Sports", new SportsCategory());
+		this.put("Technology", new TechnologyCategory());
 		/*******************************************/
 		initCategories();
 	}
